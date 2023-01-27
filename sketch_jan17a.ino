e5838cab368a443a7b00d54234d1e0a5cc3e59ff
saccdsstudent <123584309+saccdsstudent@users.noreply.github.com>
2023-01-27 12:11:36 -0800

    Add files via upload

---
+++ sketch_jan17a.ino
@@ -0,0 +1,35 @@
+/*  Spin DC motor channel 1 for 5 seconds, then coast to a stop.
+ *  After stopping, wait for 2 seconds, spin in opposite direction.
+ *  Continue to repeat until RED reset button is pressed.
+*/
+
+  #include <PRIZM.h>    // include the PRIZM library in the sketch
+  PRIZM prizm;          // instantiate a PRIZM object “prizm” so we can use its functions
+  int x = 25;            // initialize variable at one
+
+void setup() {        
+
+  prizm.PrizmBegin();   // Initialize the PRIZM controller
+  Serial.begin(9600);
+
+}
+
+void loop() {     // repeat in a loop
+
+  prizm.setMotorPower(1,x);     // spin Motor 1 CW at 25% power
+  prizm.setMotorPower(2,x);     // spin Motor 1 CW at 25% power
+  delay(1000);                  // wait while the motor runs for 5 seconds
+  prizm.setMotorPower(1,0);     // stop motor (coast to stop)
+  prizm.setMotorPower(2,0);     // stop motor (coast to stop)
+  delay(2000);                  // After stopping, wait here for 2 seconds
+  prizm.setMotorPower(1,-x);    // spin Motor 1 CCW at 25% power
+  prizm.setMotorPower(2,-x);    // spin Motor 1 CCW at 25% power
+  delay(1000);                  // wait while the motor runs for 5 seconds
+  prizm.setMotorPower(1,0);     // stop motor (coast to stop)
+  prizm.setMotorPower(2,0);     // stop motor (coast to stop)
+  delay(2000);                  // After stopping, wait here for 2 seconds, then repeat
+  Serial.println(x);
+  x=x+1;
+ 
+  
+}
